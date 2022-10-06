25396e719783b8e30a6f3c83e67f21263ddc6739
Daniel Calle <nieled@riseup.net>
2022-10-05 23:35:36 -0500

    Initial commit

---
+++ src/main/java/net/nieled/rmmexercise/RmmExerciseApplication.java
@@ -0,0 +1,13 @@
+package net.nieled.rmmexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmmExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmmExerciseApplication.class, args);
+	}
+
+}
