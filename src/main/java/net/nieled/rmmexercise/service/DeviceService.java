d4b04c29e059366f6ec8acb0ece2028e3371e41f
Daniel Calle <nieled@riseup.net>
2022-10-09 20:28:26 -0500

    define DeviceService methods

--- src/main/java/net/nieled/rmmexercise/service/DeviceService.java
+++ src/main/java/net/nieled/rmmexercise/service/DeviceService.java
@@ -1,8 +1,40 @@
 package net.nieled.rmmexercise.service;
 
+import net.nieled.rmmexercise.domain.Device;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface DeviceService {
 
+    /**
+     * Saves a device
+     *
+     * @param device the entity to save.
+     * @return the persisted entity.
+     */
+    Device save (Device device);
+
+    /**
+     * Updates a device
+     *
+     * @param device the entity to update.
+     * @return the persisted entity.
+     */
+    Device update (Device device);
+
+    /**
+     * Get all Devices
+     *
+     * @return the list of devices.
+     */
+    List<Device> findAll();
+
+    /**
+     * Delete the "id" device.
+     *
+     * @param id the id of the device.
+     */
+    void delete(Long id);
 }
